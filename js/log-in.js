feb6109bd66770ab6ca4ae563bf55a33a168ad5f
Eliana Rodriguez <eli.rodriguez0895@gmail.com>
2023-09-19 13:15:23 -0300

    login

--- js/log-in.js
+++ js/log-in.js
@@ -1,30 +1,40 @@
-const inputUser = document.querySelector("#user"),
-    inputPass = document.querySelector("#pass"),
-    check = document.querySelector("#check"),
-    formulario = document.querySelector("#form-login");
+const check=document.querySelector("#check");
+const formulario= document.querySelector("#form-login");
+const inputUser = document.querySelector("#user").value;
+const inputPass = document.querySelector("#pass").value;
 
-function registrar(valor) {
+function registrar() {
+
+
     const user = { usuario: inputUser.value, pass: inputPass.value };
-    valor === "localStorage" &&
-        localStorage.setItem("user", JSON.stringify(user));
-    valor === "sessionStorage" &&
-        sessionStorage.setItem("user", JSON.stringify(user));
+    localStorage.setItem("user", JSON.stringify(user))
 }
 
 formulario.addEventListener("submit", (e) => {
     e.preventDefault();
-    check.click ? registrar("localStorage") : registrar("sessionStorage");
-    window.location = "log-in.html"
+    registrar();
+    setTimeout(() => {
+        window.location= "log-in.html";
+    },2000)
 });
 
 
 function iniciarSesion() {
-    const verify = !!user.find(element => element.inputUser === usuario && element.inputPass === pass)
 
-    if (verify) {
+    const usuario=email.value;
+    const pass=contraseña.value;
+    const userFromStorage=JSON.parse(localStorage.getItem("user"));
+    
+    if (userFromStorage && userFromStorage.usuario === usuario && userFromStorage.pass === pass) {
         window.location = "index.html"
-        alert('¡Correo y contraseña correctos!');
     } else {
         alert('Correo o contraseña incorrecta');
     }
 }
+
+check.addEventListener("click",(e) =>{
+    iniciarSesion();
+    setTimeout(() =>{
+        window.location="index.html"
+    },2000)
+});
